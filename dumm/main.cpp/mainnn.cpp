bc9e6d45fb0f53a4b2cecda50d7833614121c41d
HAMMADQAdir <f1673673@gmail.com>
2024-05-31 14:27:36 +0530

    deleting all

--- dumm/main.cpp/mainnn.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    print("thius line is added byu sadique")
-        // ye to maine online github pe add kiya tha
-    return 0;
-}
--- kotlin/main.kt
+++
@@ -1,3 +0,0 @@
-fun foo() = {}
-//sadiqeu
-println("foo")
